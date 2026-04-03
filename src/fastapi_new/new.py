b1f9de5d433e02894971b0ebbf7cc3416a84020c
Alejandra <alejsdev@gmail.com>
2026-04-03 12:36:37 +0200

    📝 Remove login command from README instructions

--- src/fastapi_new/new.py
+++ src/fastapi_new/new.py
@@ -45,7 +45,6 @@ Visit http://localhost:8000
 > FastAPI Cloud is currently in private beta. Join the waitlist at https://fastapicloud.com
 
 ```bash
-uv run fastapi login
 uv run fastapi deploy
 ```
 
